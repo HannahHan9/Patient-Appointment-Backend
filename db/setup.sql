43e2fd1e8d219c6fab70a65c19f58624da754522
HannahHan9 <h.han9204@gmail.com>
2023-10-01 14:07:11 +0100

    initial commit

---
+++ db/setup.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS patient_appointments_test;
+DROP DATABASE IF EXISTS patient_appointments;
+
+CREATE DATABASE patient_appointments_test;
+CREATE DATABASE patient_appointments;
