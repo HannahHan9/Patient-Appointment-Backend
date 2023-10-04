4d3825a4c9431c791f3f8984292a10d543b2ed02
HannahHan9 <h.han9204@gmail.com>
2023-10-04 09:17:40 +0100

    update app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const {
+    getPatientByNhsNumber,
+    postPatients,
+    patchPatientsByNhsNumber, deletePatientByNhsNumber
+    // getAppointmentByPatient,
+} = require("./controllers");
+const {
+    handlePsqlErrors,
+    handleCustomErrors,
+    handleServerErrors,
+} = require("./errors");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/patients/:nhs_number", getPatientByNhsNumber);
+
+app.post("/api/patients", postPatients);
+
+app.patch("/api/patients/:nhs_number", patchPatientsByNhsNumber);
+
+// app.delete("/api/patients/:nhs_number", deletePatientByNhsNumber);
+
+// app.get("/api/appointments/:patient", getAppointmentByPatient);
+
+// app.post("/api/appointments/:patient", postAppointmentsByPatient);
+
+// app.patch("/api/appointments/:patient", patchAppointmentsByPatient)
+
+
+app.use(handlePsqlErrors);
+
+app.use(handleCustomErrors);
+
+app.use(handleServerErrors);
+
+module.exports = app;
