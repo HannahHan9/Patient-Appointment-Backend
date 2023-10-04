3ac31f453344f8171316af3e0a5116c4a3e51862
HannahHan9 <h.han9204@gmail.com>
2023-10-04 10:45:31 +0100

    add errors.js

---
+++ errors.js
@@ -0,0 +1,20 @@
+exports.handlePsqlErrors = (err, req, res, next) => {
+    // if (err.code === "22001") {
+    //     res.status(400).send({ msg: "Invalid Name" });
+    // } else
+    if (err.code === "23502") {
+        res.status(400).send({ msg: "Invalid Name" });
+    } else {
+        next(err);
+    }
+};
+
+exports.handleCustomErrors = (err, req, res, next) => {
+    if (err.msg) {
+        res.status(err.status).send({ msg: err.msg });
+    } else next(err);
+};
+
+exports.handleServerErrors = (err, req, res, next) => {
+    res.status(500).send({ msg: "Internal Server Error" });
+};
